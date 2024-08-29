be9333b7bf19481e57b64040f026d4aeb0f60eb2
J-Naish <mjyrgd2332@i.softbank.jp>
2024-08-30 06:28:55 +0900

    define style constants

---
+++ lib/screens/constants/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0x034AA6);
+const secondary = Color(0xBF4904);
+const tertiary = Color(0xFFFFFF);
+const quaternary = Color(0xD8D8DB);
+const quinary = Color(0x000000);
+
+const background = tertiary;
+const text = quinary;
+const textDescription = quaternary;
+const textOnFill = tertiary;
+
+const error = Color(0xEC0000);
+const warning = Color(0xEC0000);
+const success = Color(0x00A86B);
