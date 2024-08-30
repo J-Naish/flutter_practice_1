a0770d462d650acbf9fe332fa9a1382a8b811371
J-Naish <mjyrgd2332@i.softbank.jp>
2024-08-31 05:11:08 +0900

    place the main large title text

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,15 +7,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: Header(height: 64),
+    return Scaffold(  // Removed const from here
+      appBar: const Header(height: 64),
       body: Center(
         child: Column(
-          children: const [
-            TabMenu(),
+          children: [
+            const TabMenu(),
             Expanded(
-              child: Center(
-                child: Placeholder(),
+              child: Container(
+                width: double.infinity,
+                color: Colors.white,
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 32),
+                    Padding(
+                      padding: EdgeInsets.only(left: 128.0),
+                      child: Text(
+                        '人気のコンテンツ',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
