382d62a160e960ce61602b8290f48c77a00bb9c2
J-Naish <mjyrgd2332@i.softbank.jp>
2024-08-30 15:28:00 +0900

    define routes

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      initialRoute: '/',
       routes: {
+        '/': (context) => const HomePage(),
         '/sign-in': (context) => const SignInPage(),
       },
     );
