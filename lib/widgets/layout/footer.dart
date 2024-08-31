5dfaeb43327c9754521b33d01bd4c9aed5552c0a
J-Naish <mjyrgd2332@i.softbank.jp>
2024-09-01 06:12:50 +0900

    complete footer

--- lib/widgets/layout/footer.dart
+++ lib/widgets/layout/footer.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practice_1/constants/styles.dart';
 import '../../widgets/primitives/logo.dart';
 
+
 class Footer extends StatelessWidget {
   const Footer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Footer1(),
+        Footer2(),
+      ],
+    );
+  }
+}
+
+
+class Footer1 extends StatelessWidget {
+  const Footer1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +93,7 @@ class FooterNavigationItem extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        Text(title, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800])),
         const SizedBox(height: 8),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -89,6 +105,57 @@ class FooterNavigationItem extends StatelessWidget {
           ).toList(),
         ),
       ],
+    );
+  }
+}
+
+
+
+class Footer2 extends StatelessWidget {
+  const Footer2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 32),
+      decoration: const BoxDecoration(
+        color: Colors.black,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('クッキー設定', style: TextStyle(color: Colors.white, fontSize: 14)),
+              SizedBox(height: 8),
+              Text('ヘルプとサポート', style: TextStyle(color: Colors.white, fontSize: 14)),
+              SizedBox(height: 8),
+              Text('アクセシビリティ', style: TextStyle(color: Colors.white, fontSize: 14)),
+              SizedBox(height: 8),
+              Text('特定商取引法に基づく表記', style: TextStyle(color: Colors.white, fontSize: 14)),
+              SizedBox(height: 24),
+              Logo(),
+            ],
+          ),
+          Container(
+            width: 88,
+            padding: const EdgeInsets.all(8),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: const Row(
+              children: [
+                Icon(Icons.circle, color: Colors.black, size: 20),
+                SizedBox(width: 4),
+                Text("日本語"),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
