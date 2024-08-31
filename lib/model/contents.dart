4a48b8dd0f03418e286c60d8e1bc171225ee8780
J-Naish <mjyrgd2332@i.softbank.jp>
2024-08-31 21:18:21 +0900

    create a model for contents

---
+++ lib/model/contents.dart
@@ -0,0 +1,13 @@
+class Content {
+  final String title;
+  final int price;
+  final double rating;
+  final int reviewCount;
+
+  const Content({
+    required this.title,
+    required this.price,
+    required this.rating,
+    required this.reviewCount,
+  });
+}
