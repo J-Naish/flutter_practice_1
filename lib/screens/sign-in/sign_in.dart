827f0c62d11195a89d52ad42b82e216d4c45ebce
J-Naish <mjyrgd2332@i.softbank.jp>
2024-09-02 18:50:32 +0900

    use screen template for sign in

--- lib/screens/sign-in/sign_in.dart
+++ lib/screens/sign-in/sign_in.dart
@@ -1,40 +1,15 @@
 import 'package:flutter/material.dart';
+import '../../widgets/layout/screen_template.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/');
-            },
-            child: const Text(
-              'Home',
-              style: TextStyle(
-                color: Colors.black
-              )
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/sign-in');
-            },
-            child: const Text(
-              'Sign In',
-              style: TextStyle(
-                color: Colors.black
-              )
-            ),
-          ),
-        ]
-      ),
-      body: const Center(
-        child: Placeholder(),
-      ),
+    return const ScreenTemplate(
+      body: [
+        Placeholder(),
+      ]
     );
   }
 }
