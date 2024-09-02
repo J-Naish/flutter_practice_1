3ea9e40e1fabb44ef684e28721a505715312ad9a
J-Naish <mjyrgd2332@i.softbank.jp>
2024-09-02 22:10:26 +0900

    move dummy contents to model

--- lib/model/contents.dart
+++ lib/model/contents.dart
@@ -11,3 +11,227 @@ class Content {
     required this.reviewCount,
   });
 }
+
+
+const popularContents = [
+  Content(
+    title: '動画編集講座',
+    price: 1200,
+    rating: 4.2,
+    reviewCount: 120,
+  ),
+  Content(
+    title: 'イラストレーション講座',
+    price: 1500,
+    rating: 4.5,
+    reviewCount: 150,
+  ),
+  Content(
+    title: 'Webデザイン講座',
+    price: 1800,
+    rating: 4.8,
+    reviewCount: 180,
+  ),
+  Content(
+    title: 'プログラミング講座',
+    price: 2000,
+    rating: 4.9,
+    reviewCount: 200,
+  ),
+  Content(
+    title: '英語学習講座',
+    price: 2200,
+    rating: 4.7,
+    reviewCount: 220,
+  ),
+];
+
+
+const recommendedContents = [
+  Content(
+    title: 'Flutter入門',
+    price: 4800,
+    rating: 4.8,
+    reviewCount: 2321,
+  ),
+  Content(
+    title: 'Dart入門',
+    price: 3800,
+    rating: 4.5,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Python入門',
+    price: 4200,
+    rating: 3.2,
+    reviewCount: 21,
+  ),
+  Content(
+    title: 'Django入門',
+    price: 1900,
+    rating: 2.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Ruby入門',
+    price: 2800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Ruby on Rails入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'JavaScript入門',
+    price: 2800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'React入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Vue.js入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Angular入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'TypeScript入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Go入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Rust入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Kotlin入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Swift入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'iOSアプリ開発入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Androidアプリ開発入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'AWS入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'GCP入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Azure入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Firebase入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Docker入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'Kubernetes入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'CI/CD入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'TDD入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'DDD入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'クリーンアーキテクチャ入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'マイクロサービス入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'サーバーレス入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'モノリスからマイクロサービスへ',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+  Content(
+    title: 'モバイルファースト入門',
+    price: 3800,
+    rating: 4.1,
+    reviewCount: 321,
+  ),
+];
--- lib/screens/home/popular_contents_section.dart
+++ lib/screens/home/popular_contents_section.dart
@@ -3,38 +3,6 @@ import '../../model/contents.dart';
 import '../../widgets/primitives/content_card.dart';
 import './section.dart';
 
-const popularContents = [
-  Content(
-    title: '動画編集講座',
-    price: 1200,
-    rating: 4.2,
-    reviewCount: 120,
-  ),
-  Content(
-    title: 'イラストレーション講座',
-    price: 1500,
-    rating: 4.5,
-    reviewCount: 150,
-  ),
-  Content(
-    title: 'Webデザイン講座',
-    price: 1800,
-    rating: 4.8,
-    reviewCount: 180,
-  ),
-  Content(
-    title: 'プログラミング講座',
-    price: 2000,
-    rating: 4.9,
-    reviewCount: 200,
-  ),
-  Content(
-    title: '英語学習講座',
-    price: 2200,
-    rating: 4.7,
-    reviewCount: 220,
-  ),
-];
 
 class PopularContentsSection extends StatelessWidget {
   const PopularContentsSection({Key? key}) : super(key: key);
